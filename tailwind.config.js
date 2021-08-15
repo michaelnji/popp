52c46a3cc85c4d8785964837a3a6fbba292b6248
Michael <michaelnji34@gmail.com>
2021-08-15 22:09:46 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const production = !process.env.ROLLUP_WATCH;
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: {
+    content: ["./src/**/*.svelte"],
+    options: {
+      safelist: [/data-theme$/],
+    },
+    enabled: production,
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+};
